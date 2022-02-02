c67ca695965c851407db163d71843f63d236cd26
vicuartas230 <vicuartas230@gmail.com>
2022-02-02 11:01:58 -0500

    Upload readme file and script of task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This script defines a function add. """
+
+
+def add(a: float, b: float) -> float:
+    """ This function takes a float a and a float b as
+        arguments and returns their sum as a float. """
+    return a + b
