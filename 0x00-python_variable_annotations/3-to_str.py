ab81c51d97ec8300f04a2862ce7a45a06dd0a441
vicuartas230 <vicuartas230@gmail.com>
2022-02-02 11:25:28 -0500

    Upload script of task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This script defines a function to_str """
+
+
+def to_str(n: float) -> str:
+    """ This function takes a float n as argument and
+        returns the string representation of the float. """
+    return str(n)
