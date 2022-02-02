c285a47ccd77d959d53e91856b5c5be417d42d2b
vicuartas230 <vicuartas230@gmail.com>
2022-02-02 13:08:21 -0500

    Change str by float

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, str]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ This function takes a list mxd_lst of integers and
         floats and returns their sum as a float. """
     acum = 0
