8a5b0f7f250caafddd406c697bb3880de963a8f4
vicuartas230 <vicuartas230@gmail.com>
2022-02-02 11:15:42 -0500

    Upload script of task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This script defines a floor function. """
+
+
+def floor(n: float) -> int:
+    """ This function  takes a float n as argument
+        and returns the floor of the float. """
+    return int(n)
