9c045c1bb55bcb512a85537a28083c51307c615b
vicuartas230 <vicuartas230@gmail.com>
2022-02-02 12:19:42 -0500

    Upload script of task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This script defines a function sum_mixed_list. """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, str]]) -> float:
+    """ This function takes a list mxd_lst of integers and
+        floats and returns their sum as a float. """
+    acum = 0
+    for n in mxd_lst:
+        acum += n
+    return acum
