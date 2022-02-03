c70b671e9d0a03416515f5501cca30abd66153ba
vicuartas230 <vicuartas230@gmail.com>
2022-02-02 21:12:56 -0500

    Upload script of task 11

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" This script defines a function safely_get_value. """
+
+
+from typing import Any, Mapping, TypeVar, Union
+
+
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[TypeVar('T'),
+                                    None] = None) -> Union[Any, TypeVar('T')]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
