d879e8f026e171a4316959de3f9de679a14d1929
vicuartas230 <vicuartas230@gmail.com>
2022-08-07 15:00:30 -0500

    Upload script of task 6

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => ({
+      status: res.status,
+      value: res.value,
+    }))
+    .catch((err) => ({
+      status: err.status,
+      value: err,
+    }));
+}
