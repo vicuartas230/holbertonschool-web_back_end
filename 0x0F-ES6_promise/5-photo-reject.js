3624e409ba20a92beed59f2d6890a240afc5ee80
vicuartas230 <vicuartas230@gmail.com>
2022-08-07 13:18:33 -0500

    Change method to return promise

--- 0x0F-ES6_promise/5-photo-reject.js
+++ 0x0F-ES6_promise/5-photo-reject.js
@@ -1,6 +1,3 @@
 export default function uploadPhoto(filename) {
-  const promise = new Promise((resolve, reject) => {
-    reject(new Error(`${filename} cannot be processed`));
-  });
-  return promise;
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
