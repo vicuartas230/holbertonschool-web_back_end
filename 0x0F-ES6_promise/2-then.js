904b20e14519b105e413fa9db70fbf0e93136c52
vicuartas230 <vicuartas230@gmail.com>
2022-08-07 09:41:56 -0500

    Add finally

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 function handleResponseFromAPI(promise) {
   promise
     .then(() => {
-      console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
     .catch(() => {
-      console.log('Got a response from the API');
       return new Error();
+    }).finally(() => {
+        console.log('Got a response from the API');
     });
 }
 
