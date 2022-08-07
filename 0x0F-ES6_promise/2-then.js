7d57cb5f79e27205ddf94b62a16a377eaf9053eb
vicuartas230 <vicuartas230@gmail.com>
2022-08-07 09:45:17 -0500

    Fix eslint errors

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,12 +1,8 @@
 function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      return new Error();
-    }).finally(() => {
-        console.log('Got a response from the API');
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error()).finally(() => {
+      console.log('Got a response from the API');
     });
 }
 
