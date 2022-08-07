f45aad862edc153815a1f3dc2f3e067fd14d42c8
vicuartas230 <vicuartas230@gmail.com>
2022-08-07 15:13:08 -0500

    Add retur to promise

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => ({
       status: res.status,
       value: res.value,
