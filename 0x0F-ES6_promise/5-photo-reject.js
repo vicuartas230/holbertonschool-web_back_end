42b2b7c30e1c6eb0c8f7b46e747d0e2b51787029
vicuartas230 <vicuartas230@gmail.com>
2022-08-07 13:15:41 -0500

    Add object error

--- 0x0F-ES6_promise/5-photo-reject.js
+++ 0x0F-ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 export default function uploadPhoto(filename) {
-  const promise = new Promise((reject) => {
-    reject(`${filename} cannot be processed`);
+  const promise = new Promise((resolve, reject) => {
+    reject(new Error(`${filename} cannot be processed`));
   });
   return promise;
 }
