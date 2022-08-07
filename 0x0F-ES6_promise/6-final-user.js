38d8c70c661e1b1ce61dc6c09aaf1534e349e4c7
vicuartas230 <vicuartas230@gmail.com>
2022-08-07 15:51:53 -0500

    Change method

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,13 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => ({
-      status: res.status,
-      value: res.value,
+      status: res[0].status,
+      value: res[0].value,
     }))
     .catch((err) => ({
-      status: err.status,
-      value: err,
+      status: err[1].status,
+      value: err[1].reason,
     }));
 }
